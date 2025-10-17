f83877cbaca7e3a41da4421c192f03f5563df14f
Sriraman S. <sriraman.iyengar@broadcom.com>
2025-10-17 11:04:02 +0530

    fix: 🐛 Windows builds show missing package list error
    Package list json generation is not applicable for windows based nodes.
    
    Signed-off-by: Sriraman S. <sriraman.iyengar@broadcom.com>

--- scripts/tkg_byoi.py
+++ scripts/tkg_byoi.py
@@ -236,35 +236,38 @@ def copy_ova(args):
     print("Copying OVA from {} to {}".format(old_path, new_path))
     shutil.copyfile(old_path, new_path)
 
-    # Copy the package list
-    old_path = os.path.join(default_ova_destination_folder, "package_list.json")
-    new_path = os.path.join(args.ova_destination_folder, "package_list.json")
-    print("Copying package list file from {} to {}".format(old_path, new_path))
-    shutil.copyfile(old_path, new_path)
+    # Do the below only for linux based OSes
+    # We are assuming here that if its not windows based, its linux based (since we do not generate for MacOS)
+    if not args.os_type.startswith("windows"):
+        # Copy the package list
+        old_path = os.path.join(default_ova_destination_folder, "package_list.json")
+        new_path = os.path.join(args.ova_destination_folder, "package_list.json")
+        print("Copying package list file from {} to {}".format(old_path, new_path))
+        shutil.copyfile(old_path, new_path)
 
-    # Copy the kernel config file
-    old_path = os.path.join(default_ova_destination_folder, "kernel.config")
-    new_path = os.path.join(args.ova_destination_folder, "kernel.config")
-    print("Copying kernel config file from {} to {}".format(old_path, new_path))
-    shutil.copyfile(old_path, new_path)
+        # Copy the kernel config file
+        old_path = os.path.join(default_ova_destination_folder, "kernel.config")
+        new_path = os.path.join(args.ova_destination_folder, "kernel.config")
+        print("Copying kernel config file from {} to {}".format(old_path, new_path))
+        shutil.copyfile(old_path, new_path)
 
-    # Copy the OS manifest file
-    old_path = os.path.join(default_ova_destination_folder, "os_manifest.json")
-    new_path = os.path.join(args.ova_destination_folder, "os_manifest.json")
-    print("Copying OS manifest file from {} to {}".format(old_path, new_path))
-    shutil.copyfile(old_path, new_path)
+        # Copy the OS manifest file
+        old_path = os.path.join(default_ova_destination_folder, "os_manifest.json")
+        new_path = os.path.join(args.ova_destination_folder, "os_manifest.json")
+        print("Copying OS manifest file from {} to {}".format(old_path, new_path))
+        shutil.copyfile(old_path, new_path)
 
-    # Copy the Kernel Tunable details
-    old_path = os.path.join(default_ova_destination_folder, "kernel_tunables.tgz")
-    new_path = os.path.join(args.ova_destination_folder, "kernel_tunables.tgz")
-    print("Copying kernel tunables from {} to {}".format(old_path, new_path))
-    shutil.copyfile(old_path, new_path)
+        # Copy the Kernel Tunable details
+        old_path = os.path.join(default_ova_destination_folder, "kernel_tunables.tgz")
+        new_path = os.path.join(args.ova_destination_folder, "kernel_tunables.tgz")
+        print("Copying kernel tunables from {} to {}".format(old_path, new_path))
+        shutil.copyfile(old_path, new_path)
 
-    # Copy the Source repo details
-    old_path = os.path.join(default_ova_destination_folder, "repo_sources.tgz")
-    new_path = os.path.join(args.ova_destination_folder, "repo_sources.tgz")
-    print("Copying repo source details from {} to {}".format(old_path, new_path))
-    shutil.copyfile(old_path, new_path)
+        # Copy the Source repo details
+        old_path = os.path.join(default_ova_destination_folder, "repo_sources.tgz")
+        new_path = os.path.join(args.ova_destination_folder, "repo_sources.tgz")
+        print("Copying repo source details from {} to {}".format(old_path, new_path))
+        shutil.copyfile(old_path, new_path)
         
     print("Copying completed")
 
